fb124295084ff5e695504a37dd4e0f93949505a2
Yuuki <806393858@qq.com>
2019-06-17 21:49:46 +0800

    菜单增删改查还有托拉拽实现

--- src/api/sys/menu/menu.js
+++ src/api/sys/menu/menu.js
@@ -6,3 +6,26 @@ export const fetchMenus = () => {
     method: 'get'
   })
 }
+
+export const addMenu = (menu) => {
+  return axios.request({
+    url: '/menu',
+    method: 'post',
+    data: menu
+  })
+}
+
+export const editMenu = (menu) => {
+  return axios.request({
+    url: '/menu',
+    method: 'put',
+    data: menu
+  })
+}
+
+export const deleteMenu = (menuId) => {
+  return axios.request({
+    url: `/menu/${menuId}`,
+    method: 'delete'
+  })
+}
--- src/views/sys/menu/Menu.vue
+++ src/views/sys/menu/Menu.vue
@@ -2,56 +2,57 @@
   <div>
     <el-card class="box-card">
       <el-row :gutter="16">
-        <el-col :span="8" style="border-right: #c0ccda solid 1px">
+        <el-col :span="6">
           <el-dropdown>
             <el-button type="primary" icon="el-icon-plus">新建菜单</el-button>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>顶级菜单</el-dropdown-item>
-              <el-dropdown-item>子菜单</el-dropdown-item>
+              <el-dropdown-item @click.native="showAddMenu('top')">顶级菜单</el-dropdown-item>
+              <el-dropdown-item @click.native="showAddMenu('child')">子菜单</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
           <el-popover
             placement="top"
-            width="160"
+            width="200"
             v-model="visible">
-            <p>确定删除吗？</p>
+            <p>删除父菜单对应子菜单将转为顶级菜单!</p>
             <div style="text-align: right; margin: 0">
               <el-button size="mini" @click="visible = false">取消</el-button>
-              <el-button type="warning" size="mini" @click="visible = false">确定</el-button>
+              <el-button type="warning" size="mini" @click="handleDeleteMenu">确定</el-button>
             </div>
             <el-button icon="el-icon-delete" slot="reference">删除</el-button>
           </el-popover>
           <el-tree
             style="margin-top: 8px"
+            draggable
             :data="menuTreeData"
-            node-key="deptId"
-            ref="deptTree"
+            node-key="menuId"
+            ref="menuTree"
             @current-change="handleCurrentKeyChange"
+            @node-drop="handleDropComplete"
             :highlight-current="true"
             :expand-on-click-node="false"
+            :allow-drop="(draggingNode, dropNode, type)=>  dropNode.data.menuType!==2"
             :props="{children: 'children',label: 'menuName'}">
           </el-tree>
-
-
         </el-col>
 
-        <el-col :span="16">
+        <el-col :span="16"  style="border-left: #c0ccda solid 1px">
           <!--          <span style="font-size: 25px">编辑菜单</span>-->
           <el-row type="flex" justify="center">
             <el-form ref="menuForm" :model="menuForm" label-width="80px">
-              <el-form-item label="菜单名称">
+              <el-form-item label="菜单名称" prop="menuName">
                 <el-input v-model="menuForm.menuName"></el-input>
               </el-form-item>
-              <el-form-item label="权限标识">
+              <el-form-item label="权限标识" prop="permission">
                 <el-input v-model="menuForm.permission"></el-input>
               </el-form-item>
-              <el-form-item label="组件名称">
+              <el-form-item label="组件名称" prop="componentName">
                 <el-input v-model="menuForm.componentName"></el-input>
               </el-form-item>
-              <el-form-item label="请求路径">
+              <el-form-item label="请求路径" prop="url">
                 <el-input v-model="menuForm.url"></el-input>
               </el-form-item>
-              <el-form-item label="请求方法">
+              <el-form-item label="请求方法" prop="method">
                 <el-select placeholder="请选择" v-model="menuForm.method">
                   <el-option
                     v-for="item in requestMethod"
@@ -61,7 +62,7 @@
                   </el-option>
                 </el-select>
               </el-form-item>
-              <el-form-item label="菜单类型">
+              <el-form-item label="菜单类型" prop="menuType">
                 <el-select placeholder="请选择" v-model="menuForm.menuType">
                   <el-option
                     v-for="item in menuType"
@@ -74,7 +75,8 @@
             </el-form>
           </el-row>
           <el-row type="flex" justify="center">
-            <el-button type="primary">修改</el-button>
+            <el-button type="primary" v-if="menuForm.menuId" @click="handleEditMenu">修改</el-button>
+            <el-button type="primary" v-else @click="handleSaveMenu">保存</el-button>
           </el-row>
         </el-col>
       </el-row>
@@ -83,7 +85,7 @@
 </template>
 
 <script>
-  import { fetchMenus } from '@/api/sys/menu/menu'
+  import { fetchMenus,addMenu,editMenu,deleteMenu } from '@/api/sys/menu/menu'
   import { buildTree } from '@/utils/tools'
 
   export default {
@@ -141,10 +143,79 @@
       }
     },
     methods: {
-      handleCurrentKeyChange: function(currData, currNode) {
-        // this.showDeptData = this.deptData.filter(item=>item.parentId==currData.deptId)
-        this.menuForm = { ...currData }
-        // this.parentDept = {parentId:currData.deptId,deptName:currData.deptName}
+      handleCurrentKeyChange: function(currData) {
+        Object.keys(this.menuForm).forEach(key => {
+          this.menuForm[key]  = currData[key]
+        })
+      },
+      handleSaveMenu: function() {
+        addMenu(this.menuForm).then(res => {
+          if(res.code===200){
+            this.$message.success(res.msg)
+            this.$refs.menuForm.resetFields()
+            this._fetchMenus()
+          }else {
+            this.$message.warning(res.msg)
+          }
+        })
+      },
+      handleEditMenu: function() {
+        editMenu(this.menuForm).then(res => {
+          if(res.code===200){
+            this.$message.success(res.msg)
+            this.$refs.menuForm.resetFields()
+            this._fetchMenus()
+          }else {
+            this.$message.warning(res.msg)
+          }
+        })
+      },
+      handleDeleteMenu: function() {
+        if(this.menuForm.menuId){
+          deleteMenu(this.menuForm.menuId).then(res => {
+            if(res.code===200){
+              this.$message.success(res.msg)
+              this.$refs.menuForm.resetFields()
+              this._fetchMenus()
+            }else {
+              this.$message.warning(res.msg)
+            }
+          })
+        }else {
+          this.$message.warning("请先选择要删除的菜单节点")
+        }
+        this.visible = false
+      },
+      handleDropComplete: function(before,after,type) {
+        let menu = {}
+        menu.parentId = type==='inner'?after.data.menuId:after.data.parentId
+        menu.menuId = before.data.menuId
+        editMenu(menu).then(res => {
+          if(res.code===200){
+            this.$message.success(res.msg)
+
+          }else {
+            this.$message.warning(res.msg)
+          }
+        })
+      },
+      showAddMenu: function(type) {
+        if(type==='top') {
+          this.$refs.menuForm.resetFields()
+          this.menuForm.menuId = 0
+          this.menuForm.parentId = 0
+          this.menuForm.menuType = 1
+          this.$refs.menuTree.setCurrentNode('')
+        }else {
+          let parentId = this.menuForm.menuId
+          if(parentId){
+            this.$refs.menuForm.resetFields()
+            this.menuForm.parentId = parentId
+            this.menuForm.menuId = 0
+          }else {
+            this.$message.warning("请先选中父菜单")
+          }
+        }
       },
       _fetchMenus: function() {
         fetchMenus().then(res => {
